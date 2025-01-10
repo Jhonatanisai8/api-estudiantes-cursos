7a05f395b5c27009950051d926989e3a9d23e7c5
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 18:34:39 -0500

    configuracion inicial

---
+++ src/main/java/org/isai/api_estudiantes_cursos/ApiEstudiantesCursosApplication.java
@@ -0,0 +1,13 @@
+package org.isai.api_estudiantes_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstudiantesCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstudiantesCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/isai/api_estudiantes_cursos/ApiEstudiantesCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isai.api_estudiantes_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstudiantesCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
