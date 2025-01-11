03160bbe463d8d76270f94bfc401b4844e42b814
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-11 00:15:30 -0500

    clase StudentController: implementacion del metodo paramodificar un estudiante PUT-MAPPING

--- src/main/java/org/isai/api_estudiantes_cursos/controllers/StudentController.java
+++ src/main/java/org/isai/api_estudiantes_cursos/controllers/StudentController.java
@@ -9,7 +9,9 @@ import org.isai.api_estudiantes_cursos.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +48,20 @@ public class StudentController {
         return ResponseEntity
                 .ok(service.getStudents());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateStudent(@RequestBody Student student, @PathVariable Long id) {
+        try {
+            Student student2 = service.updateStudentByID(id, student);
+            return ResponseEntity
+                    .ok()
+                    .body(student2);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+
 }
