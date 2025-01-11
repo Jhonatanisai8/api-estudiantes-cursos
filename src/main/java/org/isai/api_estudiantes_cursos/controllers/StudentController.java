094bb8dce0a30aeeaafa94f83706dfa3fa4c7550
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 23:01:20 -0500

    clase StudentController: implementacion de metodo para guardar un estudiante (POST-MAPPING) Y para obtener los estudiantes (GET-MAPPING)

---
+++ src/main/java/org/isai/api_estudiantes_cursos/controllers/StudentController.java
@@ -0,0 +1,49 @@
+package org.isai.api_estudiantes_cursos.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.isai.api_estudiantes_cursos.exceptiones.StudentExistingException;
+import org.isai.api_estudiantes_cursos.models.Student;
+import org.isai.api_estudiantes_cursos.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @PostMapping
+    public ResponseEntity<?> saveStudent(@RequestBody Student student) {
+        try {
+            service.saveStudent(student);
+            URI locacion = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(student.getIdStudent())
+                    .toUri();
+            return ResponseEntity
+                    .created(locacion)
+                    .body(student);
+        } catch (StudentExistingException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() {
+        return ResponseEntity
+                .ok(service.getStudents());
+    }
+}
