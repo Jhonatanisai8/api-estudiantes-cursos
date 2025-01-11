fa2d91933554c3377307744f5adfa31757e52189
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 21:37:08 -0500

    clase Student: implementacion del metodo equals

--- src/main/java/org/isai/api_estudiantes_cursos/models/Student.java
+++ src/main/java/org/isai/api_estudiantes_cursos/models/Student.java
@@ -61,4 +61,53 @@ public class Student {
         this.edad = edad;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idStudent == null) ? 0 : idStudent.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((edad == null) ? 0 : edad.hashCode());
+        result = prime * result + ((courses == null) ? 0 : courses.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (idStudent == null) {
+            if (other.idStudent != null)
+                return false;
+        } else if (!idStudent.equals(other.idStudent))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (edad == null) {
+            if (other.edad != null)
+                return false;
+        } else if (!edad.equals(other.edad))
+            return false;
+        if (courses == null) {
+            if (other.courses != null)
+                return false;
+        } else if (!courses.equals(other.courses))
+            return false;
+        return true;
+    }
+
 }
